7600fa5dfac2a3ea2f3f441583e1653977b2b818
Vinicius Moraes <viniciusmoraeslima@gmail.com>
2022-04-13 17:01:14 -0300

    primeira api do curso

---
+++ Migrations/20220413183856_RequireCategory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace dotnet6Api.Migrations
+{
+    public partial class RequireCategory : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,82 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["Database:SqlServer"]);
+
+var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
+
+
+app.MapPost("/products", (ProductRequest productRequest, ApplicationDbContext context) => {
+    var category = context.Categories.Where(c => c.Id == productRequest.CategoryId).First();
+    var product = new Product {
+        Code = productRequest.Code,
+        Name = productRequest.Name,
+        Description = productRequest.Description,
+        Category = category
+    };
+    if(productRequest.Tags != null)
+    {
+        product.Tags = new List<Tag>();
+        foreach (var item in productRequest.Tags)
+        {
+            product.Tags.Add(new Tag{ Name = item});
+        }
+    }
+    context.Products.Add(product);
+    context.SaveChanges();
+    return Results.Created($"/products/{product.Id}", product.Id);
+});
+
+app.MapGet("/products/{id}", ([FromRoute] int id, ApplicationDbContext context) => {
+    var product = context.Products
+        .Include(p => p.Category)
+        .Include(p => p.Tags)
+        .Where(p => p.Id == id).First();
+    if(product != null)
+        return Results.Ok(product);
+    return Results.NotFound();
+});
+
+app.MapPut("/products/{id}", ([FromRoute] int id, ProductRequest productRequest, ApplicationDbContext context) => {
+    var product = context.Products
+        .Include(p => p.Tags)
+        .Where(p => p.Id == id).First();
+     var category = context.Categories.Where(c => c.Id == productRequest.CategoryId).First();
+
+    product.Code = productRequest.Code;
+    product.Name = productRequest.Name;
+    product.Description = productRequest.Description;
+    product.Category = category;
+    product.Tags = new List<Tag>();
+
+    if(productRequest.Tags != null)
+    {
+        product.Tags = new List<Tag>();
+        foreach (var item in productRequest.Tags)
+        {
+            product.Tags.Add(new Tag{ Name = item});
+        }
+    }
+
+    context.SaveChanges();
+    return Results.Ok();
+});
+
+app.MapDelete("/products/{id}", ([FromRoute] int id, ApplicationDbContext context) => {
+    var product = context.Products
+        .Where(p => p.Id == id).First();
+    context.Products.Remove(product);
+    context.SaveChanges();
+    return Results.Ok();
+});
+
+if(app.Environment.IsStaging())
+    app.MapGet("/configuration/database", (IConfiguration configuration) => {
+        return Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}");
+});
+
+app.Run();
+
